056cc025b8a1fc81bf678bcfa2f12e0dad0874f9
AndreaRispoli97 <Andrea9715@outlook.it>
2025-04-16 14:55:04 +0200

    add: added third bonus

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,18 +1,33 @@
 import { useEffect, useState } from "react";
+import axios from "axios"
 
 const Posts = () => {
+    const [data, setData] = useState([])
 
-    useEffect(() => {
-        console.log('mount del componente Posts');
 
-        return () => {
-            console.log('unmount Posts')
-        }
-    }, [])
+
+    function arrayData() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res => {
+                setData(res.data)
+            })
+    }
+
+    useEffect((arrayData), [])
+    console.log(data);
 
     return (
         <>
-            <div><h1>posts</h1></div>
+            <div>
+                <h1>posts</h1>
+                {data.map(({ id, title, body }) => (
+                    <div key={id}>
+                        <h2>{title}</h2>
+                        <p>{body}</p>
+                    </div>
+
+                ))}
+            </div>
         </>
     )
 
