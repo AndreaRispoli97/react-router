2fccd9f82af2f2a948c10fff5c64cc9cbf1a5341
AndreaRispoli97 <Andrea9715@outlook.it>
2025-04-17 15:48:12 +0200

    feat: add navigation links for previous and next posts in SinglePost component

--- src/pages/SinglePosts.jsx
+++ src/pages/SinglePosts.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 
 function SinglePost() {
 
     const navigate = useNavigate()
+    const [prevId, setPrevId] = useState(0)
+    const [postId, setPostId] = useState(0)
+
 
     const [card, setCard] = useState([])
 
@@ -15,6 +18,8 @@ function SinglePost() {
         axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
             .then(res => {
                 setCard(res.data)
+                setPrevId(parseInt(id) - 1)
+                setPostId(parseInt(id) + 1)
             })
             .catch(err => console.error(err))
     }
@@ -26,6 +31,8 @@ function SinglePost() {
                 <h1>{card.title}</h1>
                 <p>{card.body}</p>
             </div>
+            <Link to={`/posts/${prevId}`}>Post Precedente</Link>
+            <Link to={`/posts/${postId}`}>Post Prossimo</Link>
 
         </>
     )
